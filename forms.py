808120bf4d5a5d4eef9da3329ed3b87d92867428
Maria Shestakova <marijashestakova73@gmail.com>
2019-03-18 22:23:56 +0400

    Add CRD for characters

---
+++ forms.py
@@ -0,0 +1,30 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField, PasswordField, BooleanField, RadioField, SelectField
+from wtforms.validators import DataRequired
+
+
+class NewsCreateForm(FlaskForm):
+    name = StringField('', validators=[DataRequired()])
+    title = RadioField('', choices=[('Арбор', '-------- Арбор ----------'), ('Туг', '----------- Туг -----------'),
+                                    ('Панголины', '------- Панголины ------'), ('Фодинис', '-------- Фодинис ---------'),
+                                    ('Люди', '--------- Люди ----------')], validators=[DataRequired()])
+    content = SelectField('', choices=[('Эвендор', 'Эвендор'), ('Крагос', 'Крагос'), ('Серфилиус', 'Серфилиус'),
+                                    ('Георам', 'Георам'), ('Каварна', 'Каварна'), ], validators=[DataRequired()])
+    age = StringField('', validators=[DataRequired()])
+    info = StringField('')
+    submit = SubmitField('Создать')
+
+
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+class UserCreateForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    submit = SubmitField('Создать')
