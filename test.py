2ae37d16f7bf602f2dc341bc1742a7ce5a0b72c1
Yuriew <yuriew.alex2018@yandex.ru>
2019-03-22 11:58:31 +0400

    'Fix_2'

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ from requests import get, post, delete
 
 cookies = dict(session='Session cookies here')
 print(post('http://localhost:8000/api/v1/characters',
-           json={'title': 'Заголовок', 'name': 'Текст новости', 'city': 'aaaa',
-                 'age': 25, 'info': 'nit', 'ispublic': True, 'user_id': 1}, cookies=cookies).json())
+           json={'title': 'Туг', 'name': 'Gendalf', 'city': 'Георам',
+                 'age': 59, 'info': '', 'ispublic': True, 'user_id': 1}, cookies=cookies).json())
 print(get('http://localhost:8000/api/v1/characters').json())
-print(delete('http://localhost:8000/api/v1/characters/2', cookies=cookies).json())
+print(delete('http://localhost:8000/api/v1/characters/1', cookies=cookies).json())
 print(get('http://localhost:8000/api/v1/characters').json())
